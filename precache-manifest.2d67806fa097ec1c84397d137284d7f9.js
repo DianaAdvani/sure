473065857cd02b95854dd473eee62f965f4cd7c1
DianaAdvani <dianacadvani@gmail.com>
2020-08-27 15:59:55 -0700

    Updates

---
+++ precache-manifest.2d67806fa097ec1c84397d137284d7f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a05aeffa5f67556c04ce2d8220c04313",
+    "url": "/sure/index.html"
+  },
+  {
+    "revision": "1d49b97046810b8bced2",
+    "url": "/sure/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "520002a4c4f2a7411c05",
+    "url": "/sure/static/js/2.2e1391db.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sure/static/js/2.2e1391db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d49b97046810b8bced2",
+    "url": "/sure/static/js/main.111078c8.chunk.js"
+  },
+  {
+    "revision": "e969cd25a78b521a54bc",
+    "url": "/sure/static/js/runtime-main.fe66c4d4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sure/static/media/logo.5d5d9eef.svg"
+  }
+]);
